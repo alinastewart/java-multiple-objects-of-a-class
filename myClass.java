0339d181d25d3facb1f639338d7bb6f3f9d55828
Alina Stewart <alinastewart@Alinas-iMac.local>
2021-02-14 10:54:18 -0800

    Create multiple objects of one class to print number 7.

---
+++ myClass.java
@@ -0,0 +1,10 @@
+public class myClass {
+  int x = 7;
+
+  public static void main(String[] args) {
+    myClass myObj1 = new myClass();
+    myClass myObj2 = new myClass();
+    System.out.println(myObj1.x);
+    System.out.println(myObj2.x);
+  }
+}
